60c0b5d096960799caffb9b00392ee3e7153c0ff
fayllanera <faycamillellanera@gmail.com>
2019-03-22 00:50:19 +0800

    partial answer for number 2

---
+++ snorkel.pl
@@ -0,0 +1,38 @@
+“contestant(position, name, number, color)”. Here, position
+is in {1,2, 3, 4}, name is in {Gary, Barry, Larry, Harry}, number is
+in {1, 2, 3, 4}, and color is in {green, yellow, red, blue}. Also,
+define the predicate “beat(X, Y)” that is true whenever the position
+of contestant X is less than the position of contestant Y.
+
+%% can be ignored (?)
+contestant(position).
+contestant(name).
+contestant(number).
+contestant(color).
+
+%% can be ignored (?)
+position(1).
+position(2).
+position(3).
+position(4).
+
+%% can be ignored (?)
+name(Gary).
+name(Barry).
+name(Larry).
+name(Harry).
+
+%% can be ignored (?)
+wore(1);
+wore(2).
+wore(3).
+wore(4).
+
+%% can be ignored (?)
+color(green).
+color(yellow).
+color(red).
+color(blue).
+
+%% can be ignored (?)
+
